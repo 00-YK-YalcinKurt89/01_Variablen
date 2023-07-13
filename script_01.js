91f9f39f81b92b76ac7890375789a84eee0f5ae7
Yalcin <yalcin.coding@gmail.com>
2023-07-13 11:03:12 +0200

    Deklaration und Wertzuweisung 1 | Test in DevTools

--- script_01.js
+++ script_01.js
@@ -15,3 +15,24 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+
+/*
+let firstName, familyName;
+
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Nachnamen eingeben:");
+
+console.log(firstName + " " + familyName);
+*/
+
+
+// JS ist eine untypisierte Sprache! | untyped
+let test;
+
+// test = "Maxime";
+// test = 2;
+// test = false;
+
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
