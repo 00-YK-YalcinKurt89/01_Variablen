fecb1516a609fd51ab554c741884f159a001de37
Yalcin <yalcin.coding@gmail.com>
2023-07-13 12:11:48 +0200

    04 Beispiel - Berechnung

--- script_01.js
+++ script_01.js
@@ -34,5 +34,37 @@ let test;
 // test = false;
 
 
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+
+
+/************ 03a Variablen vs. Konstanten ************/
+
+// let firstName = "Max"; // Deklaration + Wertzuweisung
+// firstname ="Maxine";
+// console.log(firstName); // Ausgabe
+
+// const firstName = "Max"; // Variable mit KONSTANTEM Inhalt
+// firstName = "Maxine"; // KEINE neue Zuweisung zur LZ möglich! --> Fehlermeldung
+// console.log(firstName); // Ausgabe
+
+
+
+/**************** 04 Beispiel - Berechnung ***************/
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+let date = new Date(); // Die Daten kommen aus einer Systemfunktion 
+let year = date.getFullYear(); // Daten liefern lassen!
+ageJohn = 2023 - birthYearJohn;
+ageMark = 2023 - birthYearMark;
+
+
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
+
